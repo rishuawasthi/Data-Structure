149ceeadcbc75eb2f4cac3fa94866ec73a14640e
Rishu Awasthi <awasthi.rishu10@gmail.com>
2026-08-02 15:43:13 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.49%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int num=x^y;
+        int cnt=0;
+        while(num!=0){
+            if(num%2==1)
+            cnt++;
+
+            num/=2;
+        }
+        return cnt;
+    }
+};
