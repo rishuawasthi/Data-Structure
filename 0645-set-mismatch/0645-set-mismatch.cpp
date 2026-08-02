8d38edfe4911f8daaeddd4cd78bc26f09b333718
Rishu Awasthi <awasthi.rishu10@gmail.com>
2026-08-02 16:38:43 +0530

    Time: 0 ms (100%), Space: 25 MB (98.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int sum;
-        vector <int> answer;
-        sort(nums.begin(),nums.end());
-        int sum_n=0;
-        int sum_nums=0;
-        for(int i=0;i<nums.size();i++){
-            sum_n+=i+1;
-            sum_nums+=nums[i];
+        vector<int> answer;
+        sort(nums.begin(), nums.end());
+        int sum_n = 0;
+        int sum_nums = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum_n += i + 1;
+            sum_nums += nums[i];
         }
         int i;
-        for(i=1;i<nums.size();i++){
-            sum=nums[i-1]^nums[i];
-            if(sum==0)
-            break;
+        for (i = 1; i < nums.size(); i++) {
+            sum = nums[i - 1] ^ nums[i];
+            if (sum == 0)
+                break;
         }
         answer.push_back(nums[i]);
-        int diff=sum_n-sum_nums;
-        if(diff>0){
-            answer.push_back(nums[i]+diff);
-        }
-        else
-        answer.push_back(nums[i]+diff);
-        
+        int diff = sum_n - sum_nums;
+        answer.push_back(nums[i] + diff);
+
         return answer;
     }
 };
