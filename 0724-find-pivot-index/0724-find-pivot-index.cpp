e41ce9cc67459121de392fbd18e45c4c42a4a968
Rishu Awasthi <awasthi.rishu10@gmail.com>
2026-08-23 23:19:40 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.25%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+      int ls=0,flag=0;
+      int rs=accumulate(nums.begin()+1,nums.end(),0);
+      for(int i=0;i<nums.size();i++)
+      {
+        if(rs==ls)
+        {
+            flag=1;
+            return i;
+        }
+            ls+=nums[i];
+            if (i + 1 < nums.size())
+                rs -= nums[i + 1];
+
+      }
+      return -1;
+
     }
 };
