5223bab2fbdede21f463a3a7fe1a3a122a0e75a1
Rishu Awasthi <awasthi.rishu10@gmail.com>
2026-09-24 00:46:39 +0530

    Time: 2 ms (18.31%), Space: 27.4 MB (6.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
+      int n=height.size();
+      if(n<3)
+      return 0;
 
-         if (n < 3) return 0;
+      int storage =0;
 
-        vector <int> lmax(n,0);
-        vector <int> rmax(n,0);
-        lmax[0]=height[0];
-        rmax[n-1]=height[n-1];
-        for(int i=1;i<n;i++)
-        {
-            lmax[i]=max(height[i],lmax[i-1]);
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            rmax[i]=max(rmax[i+1],height[i]);
-        }
-         int sum=0;
-        for(int i=0 ; i<n ; i++)
-        {
-          sum+=(min(lmax[i],rmax[i])-height[i]);
-        }
- return sum;
+      vector <int> rightmax(n,0);
+      vector <int> leftmax(n,0);
+
+      leftmax[0]=height[0];
+      rightmax[n-1]=height[n-1];
+      
+      for(int i=1;i<n;i++){
+        leftmax[i]=max(height[i],leftmax[i-1]);
+      }
+
+      for(int i=n-2;i>=0;i--){
+        rightmax[i]=max(height[i],rightmax[i+1]);
+      }
+
+      for (int i=0;i<n;i++){
+        storage+=(min(leftmax[i],rightmax[i])-height[i]);
+      }
+      return storage;
+
     }
 };
